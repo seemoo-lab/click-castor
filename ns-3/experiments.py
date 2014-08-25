f4ee79d92e59055663bc30fc7cce0b3299e46fb1
Milan Schmittner <mschmittner@seemoo.tu-darmstadt.de>
2014-08-25 15:20:20 +0200

    Experiments now create own folders

--- ns-3/experiments.py
+++ ns-3/experiments.py
@@ -1,18 +1,23 @@
+#!/usr/bin/python
+
 import multiprocessing
 import subprocess
 import sys
+import argparse
+import os
+from datetime import datetime
 import numpy
 import scipy.stats
 
 # Settings: possible values are defined in 'castor-xcast.cc'
-runs       = range(1,11) # 20 runs
-duration   = 60.0 * 10.0 # 10 min
-clicks     = ["xcast", "xcast-promisc", "regular"]
+runs       = range(1,2) # 20 runs
+duration   = 60.0 * 1.0 # 10 min
+clicks     = ["xcast"]#, "xcast-promisc"]#, "regular"]
 networks   = ["small"]
 traffics   = ["20_1"] #, "4_5", "10_2", "20_1"]
 mobilities = ["20"]
 
-def generate_cmd():
+def generate_cmd(work_dir):
     """Generates all commands 
     """
     return [["./waf", "--run", "castor-xcast",
@@ -23,7 +28,7 @@ def generate_cmd():
             " --network="  + network +
             " --traffic="  + traffic +
             " --mobility=" + mobility +
-            " --outfile="  + "out/" + network + "-" + traffic + "-" + mobility + "-" + click + "-" + `run`
+            " --outfile="  + work_dir + network + "-" + traffic + "-" + mobility + "-" + click + "-" + `run`
             ]
            for run      in runs
            for click    in clicks
@@ -32,11 +37,11 @@ def generate_cmd():
            for mobility in mobilities
            ]
 
-def evaluate():
+def evaluate(work_dir):
     for network  in networks:
         for traffic  in traffics:
             for mobility in mobilities:
-                average_runs("out/" + network + "-" + traffic + "-" + mobility, clicks)
+                average_runs(work_dir + network + "-" + traffic + "-" + mobility, clicks)
 
 def average_runs(fileprefix, clicks):
     out_file = file(fileprefix, "w")
@@ -56,11 +61,15 @@ def average_runs(fileprefix, clicks):
                 accum = numpy.vstack((accum, numpy.array(line)))
             else:
                 accum = numpy.array(line)
+
         result = []
-        # Append mean and confidence interval for every metric
-        for i in range(accum.shape[1]):
-            m, h = mean_confidence_interval(accum[:,i])
-            result.append(`m` + " " + `h`)
+        if(accum.ndim > 1):
+            # Append mean and confidence interval for every metric
+            for i in range(accum.shape[1]):
+                m, h = mean_confidence_interval(accum[:,i])
+                result.append(`m` + " " + `h`)
+        else:
+            result = accum
         # Write results for this Click configuration
         out_file = file(fileprefix, "a")
         out_file.write(click)
@@ -83,24 +92,33 @@ def mean_confidence_interval(data, confidence=0.95):
     #h = se * scipy.stats.t._ppf((1+confidence)/2., n-1) # The confidence interval
     return m, se
 
-def main():
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--comment", help="Note to the experiment to be run")
+    args = parser.parse_args(argv)
+    comment = "_" + args.comment if args.comment else ""
+    i = datetime.now()
+    work_dir = "out/" + i.strftime('%Y-%m-%d_%H.%M.%S') + comment + "/"
+    if not os.path.exists(os.path.dirname(work_dir)):  
+        os.makedirs(os.path.dirname(work_dir)) 
+    
     print "Pre-Build experiment"
     result = subprocess.call(["./waf", "build"])
-    
+     
     if result:
         print >>sys.stderr, "Failed to build experiment, stop"
         return result
-    
+     
     print "Start experiments on " + `multiprocessing.cpu_count()` + " core(s)"
     pool = multiprocessing.Pool(None) # use 'multiprocessing.cpu_count()' cores
-    pool.map_async(subprocess.call, generate_cmd())
+    pool.map_async(subprocess.call, generate_cmd(work_dir))
     pool.close()
     pool.join()
     
     print "Evaluate experiments"
-    evaluate()
+    evaluate(work_dir)
     return 0
 
 if __name__ == '__main__':
-    sys.exit(main())
+    sys.exit(main(sys.argv[1:]))
     
