9af1f9195033ee287e199b19647da607bdbecaa0
Milan Schmittner <mschmittner@seemoo.tu-darmstadt.de>
2015-05-26 17:33:41 +0200

    Add documentation

--- elements/local/castor/castor.hh
+++ elements/local/castor/castor.hh
@@ -122,7 +122,7 @@ private:
 	static const uint8_t src_ip_anno_offset = DST_IP_ANNO_OFFSET + DST_IP_ANNO_SIZE; // = 4
 	static const uint8_t mac_ip_anno_offset = src_ip_anno_offset + DST_IP_ANNO_SIZE; // = 8
 	static const uint8_t castor_anno_offset = mac_ip_anno_offset + DST_IP_ANNO_SIZE; // = 12
-
+	static const uint8_t castor_paint_offset = castor_anno_offset + sizeof(Hash); // = 32, for documentation purposes only
 };
 
 CLICK_ENDDECLS
