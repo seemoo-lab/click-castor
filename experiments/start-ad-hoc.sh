91b3905d62e3d872758e6ba654eb859d6253bc25
Milan Schmittner <mschmittner@seemoo.tu-darmstadt.de>
2015-10-27 16:16:25 +0100

    Fix startup script

--- experiments/start-ad-hoc.sh
+++ experiments/start-ad-hoc.sh
@@ -4,17 +4,18 @@ ESSID='click-castor'
 # 5 GHz band
 #CHANNEL=149
 # 2.4 GHz band
-CHANNEL=13
+CHANNEL=14
 DEV='wlan0'
 
 killall -q click
 
-ip addr flush dev ${DEV}
+ifconfig ${DEV} down
 iwconfig ${DEV} mode ad-hoc
 iwconfig ${DEV} essid ${ESSID}
 iwconfig ${DEV} channel ${CHANNEL}
-iwconfig ${DEV} rate 12M fixed
 
-ifconfig ${DEV} down
 MYADDR=`hostname -i | awk -F '.' '{printf("10.0.0.%d", $4)}'`
 ifconfig ${DEV} ${MYADDR}/16 up
+
+iwconfig ${DEV} rate 12M fixed
+#iwconfig ${DEV} rate auto
--- experiments/start-all-click.sh
+++ experiments/start-all-click.sh
@@ -2,23 +2,25 @@
 
 EXPERIMENT_DIR=/root/click-castor/experiments
 
+# List all nodes
 NODES=`./nodeinfo.sh -t ID`
 
 REBOOT='no'
+NOCHECK='no'
 
-while getopts 'r' flag; do
+while getopts 'rn' flag; do
   case "${flag}" in
     r) REBOOT='yes' ;;
+    n) NOCHECK='yes' ;;
     *) error "Unexpected option ${flag}"
   esac
 done
 
-echo "--- START NODES (Reboot: ${REBOOT}) ---"
+if [[ ${REBOOT} == 'yes' ]]; then
+	echo "--- (Re)booting nodes ..."
 
-if [[ ${REBOOT} == 'yes' ]]
-then
 	for n in ${NODES}; do
-		echo "(Re)starting node ${n} ..."
+		echo "  ... node ${n} ..."
 		pypwrctrl off `./nodeinfo.sh -i ${n} -t PYPWR` &>/dev/null
 	done
 
@@ -29,20 +31,21 @@ then
 	done
 fi
 
-echo "Checking if nodes are up ..."
-for n in ${NODES}
-do
-	nodename=`./nodeinfo.sh -i ${n} -t DNS`
-	# ':' is no-op command
-	#while ! ping -c 1 ${nodename} &>/dev/null; do sleep 0.1; done
-	until ssh ${nodename} : &>/dev/null; do
-		sleep 1
+if [[ ${NOCHECK} == 'no' ]]; then
+	echo "--- Checking if nodes are up ..."
+	for n in ${NODES}
+	do
+		nodename=`./nodeinfo.sh -i ${n} -t DNS`
+		# ':' is no-op command
+		#while ! ping -c 1 ${nodename} &>/dev/null; do sleep 0.1; done
+		until ssh ${nodename} : &>/dev/null; do
+			sleep 1
+		done
+		echo "  ... ${nodename} is up!"
 	done
-	echo "Node ${nodename} is up!"
-done
+fi
 
-
-echo "--- START CLICK ---"
+echo "--- Starting Click ..."
 
 HOSTS=`mktemp`
 ./nodeinfo.sh -t DNS > ${HOSTS}
