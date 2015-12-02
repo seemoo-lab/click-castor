9d5ceb2bb321ba8192c76f4b8836d0d8bb4ce29b
Milan Schmittner <mschmittner@seemoo.tu-darmstadt.de>
2015-12-02 15:29:05 +0100

    Remove unnecessary packet copy

--- elements/local/castor/castor_xcast_to_unicast.cc
+++ elements/local/castor/castor_xcast_to_unicast.cc
@@ -15,13 +15,15 @@ void CastorXcastToUnicast::push(int, Packet *p) {
 	const GroupId& destination = reinterpret_cast<const GroupId&>(p->ip_header()->ip_dst);
 	if (destination.is_multicast()) {
 		const auto& destinations = map->get(destination);
-
 		for (int i = 0; i < destinations.size(); i++) {
-			WritablePacket* q = p->clone()->uniqueify();
+			WritablePacket* q;
+			if (i < destinations.size() - 1)
+				q = p->clone()->uniqueify();
+			else
+				q = p->uniqueify();
 			reinterpret_cast<NodeId&>(q->ip_header()->ip_dst) = destinations[i];
 			output(1).push(q);
 		}
-		p->kill();
 	} else {
 		// Unchanged
 		output(0).push(p);
