9b573eff601ac1679ce82f1d120e265e3b0bae71
Milan Schmittner <mschmittner@seemoo.tu-darmstadt.de>
2015-12-08 10:08:38 +0100

    Use new Crypto::hash() interface

--- elements/local/castor/castor_authenticate_pkt.cc
+++ elements/local/castor/castor_authenticate_pkt.cc
@@ -1,5 +1,5 @@
 #include <click/config.h>
-#include <click/confparse.hh>
+#include <click/args.hh>
 #include "castor_authenticate_pkt.hh"
 #include "castor_xcast.hh"
 #include "castor_anno.hh"
@@ -7,34 +7,28 @@
 CLICK_DECLS
 
 int CastorAuthenticatePkt::configure(Vector<String> &conf, ErrorHandler *errh) {
-	return cp_va_kparse(conf, this, errh,
-		"CRYPT", cpkP+cpkM, cpElementCast, "Crypto", &crypto,
-		cpEnd);
+	return Args(conf, this, errh)
+			.read_mp("Crypto", ElementCastArg("Crypto"), crypto)
+			.complete();
 }
 
 void CastorAuthenticatePkt::push(int, Packet *p) {
-
+	Hash pid;
+	crypto->hash(pid, CastorAnno::hash_anno(p));
 	bool isPidValid;
 
 	if(CastorPacket::isXcast(p)) {
 		CastorXcastPkt pkt = CastorXcastPkt(p);
-
-		const PacketId& computedPid = crypto->hash(CastorAnno::hash_anno(p));
-
-		isPidValid = computedPid == pkt.pid(0); // Pkt should only include a single pid
+		isPidValid = pid == pkt.pid(0); // Pkt should only include a single pid
 	} else {
 		CastorPkt& pkt = (CastorPkt&) *p->data();
-
-		const PacketId& computedPid = crypto->hash(CastorAnno::hash_anno(p));
-
-		isPidValid = (computedPid == pkt.pid);
+		isPidValid = pid == pkt.pid;
 	}
 
 	if (isPidValid)
 		output(0).push(p);
 	else
 		output(1).push(p); // Invalid -> discard
-
 }
 
 CLICK_ENDDECLS
