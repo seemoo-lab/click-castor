9cbd84976c86d01fe270df027046e16c8773d588
Milan Schmittner <mschmittner@seemoo.tu-darmstadt.de>
2015-12-09 15:40:30 +0100

    Slim down CastorDestClassifier

--- elements/local/castor/castor_dest_classifier.cc
+++ elements/local/castor/castor_dest_classifier.cc
@@ -1,29 +1,22 @@
 #include <click/config.h>
 #include <click/args.hh>
-#include <click/confparse.hh>
-#include <click/straccum.hh>
 #include "castor_dest_classifier.hh"
 
 CLICK_DECLS
 
 int CastorDestClassifier::configure(Vector<String> &conf, ErrorHandler *errh) {
-	if (Args(conf, this, errh)
+	return Args(conf, this, errh)
 			.read_mp("NodeId", myId)
-			.complete() < 0)
-		return -1;
-	return 0;
+			.complete();
 }
 
 void CastorDestClassifier::push(int, Packet *p) {
-
 	CastorPkt* header = (CastorPkt*) p->data();
 
-	if (myId == header->dst) {
+	if (myId == header->dst)
 		output(0).push(p);
-	} else {
+	else
 		output(1).push(p);
-	}
-
 }
 
 CLICK_ENDDECLS
--- elements/local/castor/castor_dest_classifier.hh
+++ elements/local/castor/castor_dest_classifier.hh
@@ -2,16 +2,15 @@
 #define CLICK_CASTOR_DEST_CLASSIFIER_HH
 
 #include <click/element.hh>
-#include <click/standard/addressinfo.hh>
 #include "castor.hh"
 
 CLICK_DECLS
 
 class CastorDestClassifier: public Element {
 public:
-	const char *class_name() const	{ return "CastorDestClassifier"; }
-	const char *port_count() const	{ return "1/2"; }
-	const char *processing() const	{ return PUSH; }
+	const char *class_name() const { return "CastorDestClassifier"; }
+	const char *port_count() const { return "1/2"; }
+	const char *processing() const { return PUSH; }
 	int configure(Vector<String>&, ErrorHandler*);
 
 	void push(int, Packet *);
