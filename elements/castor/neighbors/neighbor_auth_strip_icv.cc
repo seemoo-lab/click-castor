eea7af41b4eb100a85840524135d5b85fe3653be
Milan Schmittner <mschmittner@seemoo.tu-darmstadt.de>
2016-07-06 14:52:43 +0200

    Bug fix: remove correct number of ICVs

--- elements/castor/neighbors/neighbor_auth_strip_icv.cc
+++ elements/castor/neighbors/neighbor_auth_strip_icv.cc
@@ -1,11 +1,13 @@
 #include <click/config.h>
 #include "neighbor_auth_strip_icv.hh"
+#include "forwarder_list.hh"
 #include "../castor.hh"
 
 CLICK_DECLS
 
 Packet* NeighborAuthStripICV::simple_action(Packet* p) {
-	p->take(icv_BYTES);
+	const ForwarderList& fl = *reinterpret_cast<const ForwarderList*>(p->data());
+	p->take(fl.nicv * icv_BYTES);
 	return p;
 }
 
