e5e37598c4009dad4de2b398a29ebb1c6300a414
Milan Schmittner <mschmittner@seemoo.tu-darmstadt.de>
2016-12-12 21:00:00 +0100

    Fix new build scripts

--- build-castor-android.sh
+++ build-castor-android.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-ARCH=armv8-a
-
 if [ "x$ARCH" = 'x' ] ; then
     echo "You shouldn't use build-castor-android.sh directly, use build-castor-android-[arch].sh instead"
     exit 1
@@ -11,21 +9,25 @@ CLICK_DIR=`pwd`
 SODIUM_SRC_DIR=${CLICK_DIR}/libsodium
 INSTALL_DIR=${SODIUM_SRC_DIR}/libsodium-android-${ARCH}
 
+git submodule update --init
 if [ ! -d ${INSTALL_DIR} ]; then
 	echo "Installing libsodium in ${INSTALL_DIR}"
-	git submodule update --init
 	cd ${SODIUM_SRC_DIR}
+	if [ ! -e configure ]; then
+		./autogen.sh
+	fi
 	export ANDROID_NDK_HOME=${NDK_ROOT}
 	dist-build/android-${ARCH}.sh
 fi
 
 echo "Building Click with Castor"
 cd ${CLICK_DIR}
+make distclean
 export CPPFLAGS="-I${INSTALL_DIR}/include"
 export CXXFLAGS="-static"
 export LDFLAGS="-L${INSTALL_DIR}/lib"
 export CLICK_OPTIONS="--enable-castor \
-                      --disable-dynamic-linking \
+                      --enable-tools=no \
                       --disable-app --disable-aqm --disable-analysis --disable-test \
                       --disable-tcpudp --disable-icmp --disable-threads"
 android/android-${ARCH}.sh
--- build-castor-native.sh
+++ build-castor-native.sh
@@ -4,23 +4,26 @@ CLICK_DIR=`pwd`
 SODIUM_SRC_DIR=${CLICK_DIR}/libsodium
 INSTALL_DIR=${SODIUM_SRC_DIR}/libsodium-native
 
+git submodule update --init
 if [ ! -d ${INSTALL_DIR} ]; then
 	echo "Installing libsodium in ${INSTALL_DIR}"
-	git submodule update --init
 	cd ${SODIUM_SRC_DIR}
-	./configure --prefix=${INSTALL_DIR} --enable-minimal
+	if [ ! -e configure ]; then
+		./autogen.sh
+	fi
+	./configure --prefix=${INSTALL_DIR} --enable-minimal --enable-shared=no
 	make && make check
 	make install
 fi
 
 echo "Building Click with Castor"
 cd ${CLICK_DIR}
-
+make distclean
 export CPPFLAGS="-I${INSTALL_DIR}/include"
-export CXXFLAGS="-static"
 export LDFLAGS="-L${INSTALL_DIR}/lib"
 ./configure --enable-castor --enable-userlevel --disable-linuxmodule \
-            --disable-dynamic-linking \
+			--disable-dynamic-linking \
+			--enable-tools=no \
             --disable-app --disable-aqm --disable-analysis --disable-test \
             --disable-tcpudp --disable-icmp --disable-threads --disable-tools
 make
