b7f19f5e97315b6453bc7f8df7d37a537d59be98
Milan Schmittner <mschmittner@seemoo.tu-darmstadt.de>
2017-01-21 15:24:28 +0100

    Fix arm compile scripts

--- android/android-armv7-a.sh
+++ android/android-armv7-a.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export TARGET_ARCH=armv7-a
 export CFLAGS="-Os -mfloat-abi=softfp -mfpu=vfpv3-d16 -mthumb -marm -march=${TARGET_ARCH}"
-ARCH=arm HOST_COMPILER=arm-linux-androideabi "$(dirname "$0")/android-build.sh"
+NDK_API_COMPAT=21 ARCH=arm HOST_COMPILER=arm-linux-androideabi CLICK_OPTIONS="${CLICK_OPTIONS}" "$(dirname "$0")/android-build.sh"
